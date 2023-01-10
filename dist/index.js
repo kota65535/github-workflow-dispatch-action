0abc840371054b71fcab94180305644a1e18d489
Tomohiko Ozawa <kota65535@gmail.com>
2023-01-10 20:23:21 +0900

    no default ref

--- dist/index.js
+++ dist/index.js
@@ -9509,41 +9509,40 @@ function wrappy (fn, cb) {
 const core = __nccwpck_require__(2186);
 const { getOctokit } = __nccwpck_require__(5438);
 
-let octokit
+let octokit;
 
 const initOctokit = (token) => {
   octokit = getOctokit(token);
-}
+};
 
 const main = async () => {
-  const repository = core.getInput('repository').trim();
-  const workflow = core.getInput('workflow').trim();
-  const inputsJson = core.getInput('inputs').trim();
-  let ref = core.getInput('ref').trim();
-  let githubToken = core.getInput('token').trim();
+  const repository = core.getInput("repository").trim();
+  const workflow = core.getInput("workflow").trim();
+  const inputsJson = core.getInput("inputs").trim();
+  const ref = core.getInput("ref").trim();
+  let githubToken = core.getInput("token").trim();
   const defaultGithubToken = core.getInput("default-token");
 
   // if repository not given, use this repository
-  let [owner, repo] = repository.split('/');
+  const [owner, repo] = repository.split("/");
 
   githubToken = githubToken || process.env.GITHUB_TOKEN || defaultGithubToken;
   if (!githubToken) {
     throw new Error("No GitHub token provided");
   }
 
-  initOctokit(githubToken)
+  initOctokit(githubToken);
 
-  const inputs = JSON.parse(inputsJson)
+  const inputs = JSON.parse(inputsJson);
 
   const res = await octokit.rest.actions.createWorkflowDispatch({
     owner,
     repo,
     workflow_id: workflow,
     ref,
-    inputs
+    inputs,
   });
-  core.info(`${res.status}: ${res.data}`)
-
+  core.info(`${res.status}: ${res.data}`);
 };
 
 module.exports = main;
