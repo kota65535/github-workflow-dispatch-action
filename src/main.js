0f8219e1eb51eca73612242bb5de1ac5a5060b87
Tomohiko Ozawa <kota65535@gmail.com>
2023-01-10 19:01:48 +0900

    Format, lint etc. (#1)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-const { context, getOctokit } = require('@actions/github');
+const { getOctokit } = require('@actions/github');
 
 let octokit
 
@@ -7,42 +7,23 @@ const initOctokit = (token) => {
   octokit = getOctokit(token);
 }
 
-const getDefaultBranch = async (owner, repo) => {
-  const res = await octokit.rest.repos.get({
-    owner,
-    repo
-  });
-  return res.data.default_branch;
-};
-
 const main = async () => {
   const repository = core.getInput('repository').trim();
   const workflow = core.getInput('workflow').trim();
   const inputsJson = core.getInput('inputs').trim();
   let ref = core.getInput('ref').trim();
   let githubToken = core.getInput('token').trim();
+  const defaultGithubToken = core.getInput("default-token");
 
   // if repository not given, use this repository
-  let owner, repo;
-  if (repository === '') {
-    owner = context.repo.owner;
-    repo = context.repo.repo;
-  } else {
-    [owner, repo] = repository.split('/');
-  }
+  let [owner, repo] = repository.split('/');
 
-  // github token can be also given via env
-  githubToken = githubToken || process.env.GITHUB_TOKEN;
-  if (githubToken === '') {
-    throw new Error('Need to provide one of token or GITHUB_TOKEN environment variable');
+  githubToken = githubToken || process.env.GITHUB_TOKEN || defaultGithubToken;
+  if (!githubToken) {
+    throw new Error("No GitHub token provided");
   }
 
   initOctokit(githubToken)
-
-  // if ref not given, use the default branch
-  if (ref === '') {
-    ref = await getDefaultBranch(owner, repo);
-  }
 
   const inputs = JSON.parse(inputsJson)
 
